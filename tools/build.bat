29ece6c1a6fa260490fbeb4a46d13d2cec823136
Corwin <corwin_hope@yahoo.com>
2024-02-06 00:40:19 -0500

    Update

--- tools/build.bat
+++ tools/build.bat
@@ -2,6 +2,9 @@
 ECHO Build in progress...
 node "%~dp0r.js" -o "%~dp0build.js"
 :: You can change output file!!
-ECHO uglifyjs in progress... Output file: "C:\Users\Anthony\Documents\grapherDeploy\grapher\public\www-built\app.js"
-uglifyjs "%~dp0..\www-built\app.js" -c -m -o "C:\Users\Anthony\Documents\grapherDeploy\grapher\public\www-built\app.js"
-C:\Users\Anthony\Documents\grapherDeploy\grapher\public\www-built\app.js
+::ECHO uglifyjs in progress... Output file: "C:\Users\Anthony\Documents\grapherDeploy\grapher\public\www-built\app.js"
+::uglifyjs "%~dp0..\www-built\app.js" -c -m -o "C:\Users\Anthony\Documents\grapherDeploy\grapher\public\www-built\app.js"
+::C:\Users\Anthony\Documents\grapherDeploy\grapher\public\www-built\app.js
+ECHO uglifyjs in progress... Output file: "C:\Users\Anthony\Documents\grapherDev\easy-grapher\app.js"
+uglifyjs "%~dp0..\www-built\app.js" -c -m -o "C:\Users\Anthony\Documents\grapherDev\easy-grapher\app.js"
+::C:\Users\Anthony\Documents\grapherDeploy\grapher\public\www-built\app.js
