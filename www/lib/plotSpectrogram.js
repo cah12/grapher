8dece3ed8e020c7c619a1f10cbb50fccf419072a
Corwin <corwin_hope@yahoo.com>
2025-05-05 16:40:18 -0400

    Removed added

--- www/lib/plotSpectrogram.js
+++ www/lib/plotSpectrogram.js
@@ -290,7 +290,7 @@ class PlotSpectrogram extends PlotRasterItem {
      *
      */
     this.renderTile = function (xMap, yMap, tile, image) {
-      console.time();
+      //console.time();
       var range = d_data.data.interval(Static.ZAxis);
       if (!range.isValid()) return;
 
